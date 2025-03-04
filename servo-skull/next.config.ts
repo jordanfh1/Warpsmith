cfb60f6574ef1b53c60a8ae0a35eea9aea25c7c9
Jordan <jordanfhoughton@gmail.com>
2025-03-04 19:59:08 +0000

    vercel fixes

--- servo-skull/next.config.ts
+++ servo-skull/next.config.ts
@@ -4,4 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  output: 'standalone',
+};
+
+module.exports = {
+  output: 'export',
+  trailingSlash: true,
+};
+
+
 export default nextConfig;
